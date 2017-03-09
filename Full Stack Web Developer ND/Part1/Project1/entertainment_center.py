9f1495cba623bc3319e7177b645159c1cd53d50a
Nan Meng <naen.mong@gmail.com>
2017-03-09 19:07:37 +0800

    Add comments
    Add comments to the class and function

--- Full Stack Web Developer ND/Part1/Project1/entertainment_center.py
+++ Full Stack Web Developer ND/Part1/Project1/entertainment_center.py
@@ -1,39 +1,51 @@
 import fresh_tomatoes
 import media
 
-# Movies
+## Movies
 
+# Inception movie instance
 inception = media.Movie(movie_title = "Inception", movie_duration = 30,
 						movie_storyline = "A movie in dream",
 						poster_image = "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_(2010)_theatrical_poster.jpg",
 						trailer_youtube = "https://www.youtube.com/watch?v=66TuSJo4dZM")
-inception.show_trailer()
+
+# School of Rock movie instance
 school_of_rock = media.Movie(movie_title = "School of Rock", movie_duration = 30,
 							 movie_storyline = "Using rock music to learn",
 							 poster_image = "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
 							 trailer_youtube = "https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+# Ratatouille movie instance
 ratatouille = media.Movie(movie_title = "Ratatouille", movie_duration = 30,
 						  movie_storyline = "A rat is a chef in Pairs",
 						  poster_image = "http://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
 						  trailer_youtube = "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+# Midnight in Paris movie instance
 midnight_in_paris = media.Movie(movie_title = "Midnight in Paris", movie_duration = 30,
 								movie_storyline = "Going back in time to meet authors",
 								poster_image = "http://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
 								trailer_youtube = "https://www.youtube.com/watch?v=BYRWfS2s2v4")
+
+# Hunger Games in Paris movie instance
 hunger_games = media.Movie(movie_title = "Hunger Games", movie_duration = 30,
 						   movie_storyline = "A really real reality show",
 						   poster_image = "http://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
 						   trailer_youtube = "https://www.youtube.com/watch?v=PbA63a7H0bo")
+
+# Resident Evil in Paris movie instance
 resident_evil = media.Movie(movie_title = "Resident Evil 7", movie_duration = 30,
 							movie_storyline = "A survival horror movie",
 							poster_image = "http://cdn1-www.comingsoon.net/assets/uploads/gallery/resident-evil-the-final-chapter/last-resident-evil-poster.jpg",
 							trailer_youtube = "https://www.youtube.com/watch?v=jbtmW3ydOkU")
+
+# Suicide Squad in Paris movie instance
 suicide_squad = media.Movie(movie_title = "Suicide Squad", movie_duration = 30,
 							movie_storyline = "An American superhero film",
 							poster_image = "https://upload.wikimedia.org/wikipedia/en/5/50/Suicide_Squad_(film)_Poster.png",
 							trailer_youtube = "https://www.youtube.com/watch?v=mMb-RrhTxIE")
 
-# Movies list
+# Movie instances list
 movies = [resident_evil, suicide_squad, inception, 
 		  school_of_rock, ratatouille, midnight_in_paris, 
 		  hunger_games]
@@ -43,6 +55,7 @@ movies = [resident_evil, suicide_squad, inception,
 
 # TV shows
 
+# Friends tv instance
 friends = media.TvShow(tv_title = "Friends",
 					   tv_duration = 30,
 					   tv_youtube = "https://www.youtube.com/watch?v=lWqUX_aCKBE&list=PL4VjYMnxdYuVdqqAuuURtNHY5cfNFy606&index=2",
@@ -51,6 +64,7 @@ friends = media.TvShow(tv_title = "Friends",
 					   tv_episode = "https://www.youtube.com/watch?v=8mP5xOg7ijs",
 					   tv_station = "TBS")
 
+# Shield tv instance
 shield = media.TvShow(tv_title = "Shield",
 					  tv_duration = 30,
 					  tv_youtube = "https://www.youtube.com/watch?v=GlQu03jNhk0",
--- Full Stack Web Developer ND/Part1/Project1/media.py
+++ Full Stack Web Developer ND/Part1/Project1/media.py
@@ -1,7 +1,21 @@
 import webbrowser
 
 class Video():
-	""" This class provides a way to store videos related information"""
+	""" This class provides a way to store videos related information
+		
+		Attributes:
+			title : The title of video
+			duration : The time of duration of the video trailer
+			video_youtube : The URL link of video trailer on youtube
+			poster_image : The post image of the video trailer
+
+		VALID_RATINGS : There are four choices of valid ratings, namely, G, PG, PG-13, R
+
+		Functions:
+			show_trailer() : Open a weblink of video trailer
+			show_info()    : Print the infomation of video
+
+	"""
 
 	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
@@ -24,7 +38,17 @@ class Video():
 
 
 class Movie(Video):
-	""" This class provides a way to store movie related information"""
+	""" This class provides a way to store movie related information
+
+		Attributes:
+			movie_title : The title of movie
+			movie_duration : The time of duration of the movie trailer
+			movie_storyline : A brief introduction of the movie
+			poster_image : The post image of the movie trailer
+			trailer_youtube : The URL link of movie trailer on youtube
+
+
+	"""
 
 	def __init__(self, movie_title, movie_duration, movie_storyline, poster_image, trailer_youtube):
 		# self.title = movie_title
@@ -34,7 +58,19 @@ class Movie(Video):
 
 
 class TvShow(Video):
-	""" This class provides a way to store TV show related information"""
+	""" This class provides a way to store TV show related information
+		
+		Attributes:
+			tv_title     : The title of TV
+			tv_duration  : The time of duration of the TV trailer
+			poster_image : The post image of the TV trailer
+			tv_youtube   : The URL link of TV trailer on youtube
+			tv_season    : The season of TV
+			tv_episode   : The episode link of TV
+			tv_station   : The station of TV
+
+
+	"""
 
 	def __init__(self, tv_title, tv_duration, tv_youtube, poster_image, tv_season, tv_episode, tv_station):
 		Video.__init__(self, tv_title, tv_duration, tv_youtube, poster_image)
